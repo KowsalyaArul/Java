1752c6cd2ec0dc010ee5f1dd02f53a5129bba2bb
KowsalyaArul <a.kowsalya2003@gmail.com>
2023-12-09 18:40:54 +0530

    switch

---
+++ src/Switch.java
@@ -0,0 +1,21 @@
+public class Switch {
+    public static void main(String[] args)
+    {
+        char day = 5;
+        switch(day) {
+            case 1:
+                System.out.println("Sunday");
+                break;
+            case 2:
+                System.out.println("Monday");
+                break;
+            case 3:
+                System.out.println("Tuesday");
+                break;
+            default:
+                System.out.println("It is weekend");
+        }
+
+
+    }
+}
